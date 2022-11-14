34210425c458263d7270bbcb4257980bda2e0626
azhitkev <90805408+azhitkev@users.noreply.github.com>
2022-11-13 22:04:47 -0500

    work on contact page
    - google maps DOES NOT WORK bc google maps api is free/not free..confused

--- west-site/src/main.js
+++ west-site/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import VueGoogleMaps from "@fawmi/vue-google-maps";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -8,13 +9,25 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 /* import specific icons */
 import { faCopy } from "@fortawesome/free-regular-svg-icons";
+import {
+  faFacebookSquare,
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 library.add(faCopy);
+library.add(faFacebookSquare, faInstagram, faTwitter, faLinkedin);
 
 loadFonts();
 
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
+  .use(VueGoogleMaps, {
+    load: {
+      key: "AIzaSyBE0Uc-gUn3BKnoGUQTTodtgNSE0atrHWk",
+    },
+  })
   .use(router)
   .use(store)
   .mount("#app");
--- west-site/src/views/ContactView.vue
+++ west-site/src/views/ContactView.vue
@@ -9,23 +9,81 @@
       <h1 class="text-5xl font-medium tracking-widest">Contact Us</h1>
       <h2 class="text-xl fonts-semibold">Have a question? Reach out!</h2>
     </div>
-    <div id="email-group" class="text-left ml-12">
-      <p class="text-left mt-12 font-semibold text-xl">Email Us!</p>
-      <input
-        id="email-text"
-        readonly
-        class="w-80 text-center rounded border-2 border-purple-700 mt-2"
-        value="westernengineeringsatellite@gmail.com"
-      />
-      <button class="btn" v-on:click="copyEmail()">
-        <font-awesome-icon icon="fa-regular fa-copy" class="ml-2 fa-lg" />
-      </button>
+    <div class="ml-20">
+      <div id="email-group" class="text-left">
+        <p class="text-left mt-12 font-semibold text-xl">Email Us!</p>
+        <input
+          id="email-text"
+          readonly
+          class="w-80 text-center rounded border-2 border-purple-700 mt-2"
+          value="westernengineeringsatellite@gmail.com"
+        />
+        <button class="btn" v-on:click="copyEmail()">
+          <font-awesome-icon icon="fa-regular fa-copy" class="ml-2 fa-lg" />
+        </button>
+      </div>
+      <div id="social-media-group" class="text-left">
+        <p class="text-left mt-12 font-semibold text-xl">Ways to keep up:</p>
+        <button class="btn" v-on:click="openFacebook">
+          <font-awesome-icon
+            icon="fa-brands fa-square-facebook"
+            class="fa-2xl"
+          />
+        </button>
+        <button class="btn ml-2" v-on:click="openInstagram">
+          <font-awesome-icon icon="fa-brands fa-instagram" class="fa-2xl" />
+        </button>
+        <button class="btn ml-2" v-on:click="openTwitter">
+          <font-awesome-icon icon="fa-brands fa-twitter" class="fa-2xl" />
+        </button>
+        <button class="btn ml-2" v-on:click="openLinkedIn">
+          <font-awesome-icon icon="fa-brands fa-linkedin" class="fa-2xl" />
+        </button>
+      </div>
+      <div id="location-group">
+        <p>Spencer Engineering Building</p>
+        <p>The University of Western Ontario</p>
+        <p>London, Ontarion, Canada, N6A 5B9</p>
+        <Map
+          :center="center"
+          :zoom="7"
+          map-type-id="terrain"
+          style="width: 500px; height: 300px"
+        >
+          <GMapCluster>
+            <GMapMarker
+              :key="index"
+              v-for="(m, index) in markers"
+              :position="m.position"
+              :clickable="true"
+              :draggable="true"
+              @click="center = m.position"
+            />
+          </GMapCluster>
+        </Map>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { Map, GMapCluster, GMapMarker } from "@fawmi/vue-google-maps";
+
 export default {
+  components: { Map, GMapCluster, GMapMarker },
+  data() {
+    return {
+      center: { lat: 43.0055547, lng: -81.2782254 },
+      markers: [
+        {
+          position: {
+            lat: 43.0055547,
+            lng: -81.2782254,
+          },
+        },
+      ],
+    };
+  },
   methods: {
     copyEmail() {
       let text = document.getElementById("email-text").value;
@@ -36,6 +94,20 @@ export default {
       } catch (err) {
         console.error("Failed to copy: ", err);
       }
+    },
+    openFacebook() {
+      window.open("https://www.facebook.com/westernengineeringsatellite");
+    },
+    openInstagram() {
+      window.open("https://www.instagram.com/westernengineeringsatellite/");
+    },
+    openTwitter() {
+      window.open("https://twitter.com/westernengsat");
+    },
+    openLinkedIn() {
+      window.open(
+        "https://www.linkedin.com/company/western-engineering-satellite-team/"
+      );
     },
   },
 };
@@ -68,5 +140,8 @@ h2 {
 }
 #email-text[readonly] {
   cursor: not-allowed;
+}
+.btn {
+  margin-top: 8px;
 }
 </style>
