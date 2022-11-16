9bf1c2f8752ae1ae7b5cb84156fa62a64da8e442
azhitkev <90805408+azhitkev@users.noreply.github.com>
2022-11-16 01:01:07 -0500

    added button to the home page

--- west-site/src/views/HomeView.vue
+++ west-site/src/views/HomeView.vue
@@ -7,20 +7,23 @@
     <main role="main" class="text-white">
       <section id="section-1" class="flex my-20 mx-40">
         <h1 class="text-left font-medium w-2/4 mr-8">Our Mission.</h1>
-        <div class="font-semibold">
-          <p class="text-lg text-left">
+        <div class="font-semibold flex-col text-left">
+          <p class="text-lg">
             The Western Engineering Satellite Team was created by a group of
             undergraduate students from Western University passionate about
             learning and designing aerospace and satellite projects.
           </p>
           <br />
-          <p class="text-lg text-left">
+          <p class="text-lg">
             WEST strives to create more opportunities for students to gain
             experience designing and creating small satellites before heading
             out into the workplace. WEST aims to provide the STEM community with
             insight into various different areas of STEM that would otherwise be
             difficult to test.
           </p>
+          <button class="button-30 mt-8 font-semibold" role="button">
+            Learn more
+          </button>
         </div>
       </section>
       <section id="section-2"><h1>The Execs</h1></section>
@@ -205,5 +208,52 @@ footer a {
 }
 html {
   background-color: black;
+}
+
+.button-30 {
+  align-items: center;
+  appearance: none;
+  background-color: #fcfcfd;
+  border-radius: 4px;
+  border-width: 0;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: #36395a;
+  cursor: pointer;
+  display: inline-flex;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow 0.15s, transform 0.15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow, transform;
+  font-size: 16px;
+}
+
+.button-30:focus {
+  box-shadow: #d6d6e7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+}
+
+.button-30:hover {
+  box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+  transform: translateY(-2px);
+}
+
+.button-30:active {
+  box-shadow: #d6d6e7 0 3px 7px inset;
+  transform: translateY(2px);
 }
 </style>
