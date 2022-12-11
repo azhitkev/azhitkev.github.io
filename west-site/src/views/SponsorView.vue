2e8c260bb26eb9c0bde7b8869928ce3d8be37484
azhitkev <90805408+azhitkev@users.noreply.github.com>
2022-12-10 20:23:14 -0500

    form that sends email to the inputted email

--- west-site/src/views/SponsorView.vue
+++ west-site/src/views/SponsorView.vue
@@ -24,13 +24,60 @@
       <p class="text-left mt-12 mb-4 font-semibold text-xl">
         Interested in becoming a sponsor?
       </p>
-      <p class="text-left font-medium text-lg">
+      <p class="text-left font-medium text-lg mb-8">
         Please take a look at our official sponsorship package and consider
         filling out the form below!
       </p>
+      <div>
+        <form
+          id="form"
+          action="https://formsubmit.co/lex.zhch@gmail.com"
+          method="POST"
+        >
+          <a
+            href="https://dev.to/zougari47/contact-form-without-backend-using-javascript-4a8c"
+            target="_blank"
+            >blog link</a
+          >
+          <div class="input_group">
+            <label for="">name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Full Name"
+              required
+            />
+          </div>
+
+          <div class="input_group">
+            <label for="">email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Your email"
+              required
+            />
+          </div>
+
+          <div class="input_group">
+            <label for="content">Message</label>
+            <textarea
+              name="content"
+              placeholder="Your message"
+              required
+            ></textarea>
+          </div>
+
+          <div class="input_group">
+            <button type="submit">Submit Form</button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
+
 <style scoped>
 #page-header {
   position: relative;
@@ -55,5 +102,65 @@ h2 {
   align-items: center; /** Y-axis align **/
   justify-content: center; /** X-axis align */
   text-align: left;
+}
+body {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  font-family: sans-serif;
+  position: relative;
+}
+form {
+  width: 100%;
+  height: 500px;
+  padding-top: 90px;
+  box-sizing: border-box;
+  max-width: 600px;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+}
+.input_group {
+  width: 90%;
+  margin: 0 auto 1.2rem;
+  overflow: hidden;
+}
+label {
+  display: block;
+  text-transform: capitalize;
+  font-weight: 600;
+  color: #212529;
+  margin-bottom: 0.5rem;
+}
+input,
+textarea {
+  width: 90%;
+  padding: 0.5rem 1rem;
+}
+input:focus,
+textarea:focus {
+  outline: none;
+}
+button {
+  display: block;
+  width: 50%;
+  background: #0d6efd;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 0.8rem 0;
+  font-size: 1.2rem;
+  color: white;
+  margin: 0 auto;
+}
+button:hover {
+  transform: scale(0.99);
+}
+a {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 10%;
 }
 </style>
